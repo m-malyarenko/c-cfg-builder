881f541f5196808feba248005015385c83865fe2
Mikhail Malyarenko <malyarenko.md@gmail.com>
2022-11-22 17:42:07 +0300

    dev: add dummy operation entity

--- src/entity/operation.c
+++ src/entity/operation.c
@@ -1,4 +1,35 @@
+#include <stdlib.h>
+
 #include <ustring/str.h>
 #include <ustring/str_list.h>
 
 #include "operation.h"
+
+struct operation* operation_new() {
+    struct operation* new_operation = malloc(sizeof(struct operation));
+
+    new_operation->operands = str_list_new();
+    return new_operation;
+}
+
+void operation_drop(struct operation** self) {
+    if ((self == NULL) || (*self == NULL)) {
+        return;
+    }
+
+    str_list_drop(&(*self)->operands);
+    free(*self);
+    *self = NULL;
+}
+
+void operation_push_operand(struct operation* self, str_t* operand) {
+    if (self == NULL) {
+        return;
+    }
+
+    str_list_push(self->operands, operand);
+}
+
+str_t* operation_to_str(const struct operation* self) {
+    return (self == NULL) ? str_new(NULL) : str_list_join(self->operands, " ");
+}
--- src/entity/operation.h
+++ src/entity/operation.h
@@ -1,10 +1,19 @@
 #ifndef __CFG_OPERATION_H__
 #define __CFG_OPERATION_H__
 
+#include <ustring/str.h>
 #include <ustring/str_list.h>
 
 struct operation {
     str_list_t* operands;
 };
+
+struct operation* operation_new();
+
+void operation_drop(struct operation** self);
+
+void operation_push_operand(struct operation* self, str_t* operand);
+
+str_t* operation_to_str(const struct operation* self);
 
 #endif /* __CFG_OPERATION_H__ */
