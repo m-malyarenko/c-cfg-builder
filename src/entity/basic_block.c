b683ef9cdf24308685878cb367ce222f197a867b
Mikhail Malyarenko <malyarenko.md@gmail.com>
2022-11-30 23:51:53 +0300

    feat: expand basic block functionality & add ref count

--- src/entity/basic_block.c
+++ src/entity/basic_block.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#include "operation.h"
 #include "basic_block.h"
 
 struct basic_block* basic_block_new() {
     struct basic_block* new_basic_block = malloc(sizeof(struct basic_block));
+
+    new_basic_block->ref_count = 0;
 
     new_basic_block->links.len = 0;
     new_basic_block->links.buffer = NULL;
@@ -24,12 +25,16 @@ void basic_block_drop(struct basic_block** self) {
     }
 
     if ((*self)->links.buffer != NULL) {
+        for (size_t i = 0; i < (*self)->links.len; i++) {
+            (*self)->links.buffer[i]->ref_count -= 1;
+        }
+
         free((*self)->links.buffer);
     }
 
     if ((*self)->operations.buffer != NULL) {
         for (size_t i = 0; i < (*self)->operations.len; i++) {
-            operation_drop(&(*self)->operations.buffer[i]);
+            str_list_drop(&(*self)->operations.buffer[i]);
         }
 
         free((*self)->operations.buffer);
@@ -43,7 +48,7 @@ bool basic_block_is_empty(const struct basic_block* self) {
     return (self == NULL) ? true : (self->operations.len == 0);
 }
 
-void basic_block_push_operation(struct basic_block* self, struct operation* op) {
+void basic_block_push_operation(struct basic_block* self, operation_t* op) {
     if ((self == NULL) || (op == NULL)) {
         return;
     }
@@ -58,18 +63,29 @@ void basic_block_push_operation(struct basic_block* self, struct operation* op) 
     self->operations.len += 1;
 }
 
+const operation_t* basic_block_get_operation(const struct basic_block* self, size_t idx) {
+    if (self == NULL) {
+        return NULL;
+    }
+
+    assert(idx < self->operations.len);
+
+    return self->operations.buffer[idx];
+}
+
 void basic_block_resize_links(struct basic_block* self, size_t new_size) {
     if ((self == NULL) || (self->links.len == new_size)) {
         return;
     }
 
-    self->links.len = new_size;
     self->links.buffer =
         realloc(self->links.buffer, self->links.len * sizeof(struct basic_block*));
     
-    for (size_t i = self->links.len ; i < new_size; i++) {
+    for (size_t i = self->links.len; i < new_size; i++) {
         self->links.buffer[i] = NULL;
     }
+
+    self->links.len = new_size;
 }
 
 void basic_block_set_link(struct basic_block* self, struct basic_block* bb, size_t idx) {
@@ -79,27 +95,25 @@ void basic_block_set_link(struct basic_block* self, struct basic_block* bb, size
 
     assert(idx < self->links.len);
 
+    if (self->links.buffer[idx] != NULL) {
+        self->links.buffer[idx]->ref_count -= 1;
+    }
+
     self->links.buffer[idx] = bb;
+
+    if (bb != NULL) {
+        bb->ref_count += 1;
+    }
 }
 
-struct basic_block* basic_block_split_end(struct basic_block* self) {
-    if (self == NULL) {
-        return NULL;
+void basic_block_append_operations(struct basic_block* self, struct basic_block* donor) {
+    if ((self == NULL) || (donor == NULL)) {
+        return;
     }
 
-    if (self->operations.len == 0) {
-        return self;
+    const size_t donor_op_num = donor->operations.len;
+    for (size_t i = 0; i < donor_op_num; i++) {
+        operation_t* op_copy = str_list_copy(basic_block_get_operation(donor, i));
+        basic_block_push_operation(self, op_copy);
     }
-
-    struct basic_block* new_basic_block = basic_block_new();
-    basic_block_resize_links(new_basic_block, self->links.len);
-
-    for (size_t i = 0; i < self->links.len; i++) {
-        new_basic_block->links.buffer[i] = self->links.buffer[i];
-    }
-
-    basic_block_resize_links(self, 1);
-    basic_block_set_link(self, new_basic_block, 0);
-
-    return new_basic_block; 
 }
--- src/entity/basic_block.h
+++ src/entity/basic_block.h
@@ -4,13 +4,17 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-#include "operation.h"
+#include <ustring/str_list.h>
 
 #define BB_OPERATIONS_DEFAULT_CAP ((size_t) 1)
 
+typedef str_list_t operation_t;
+
 struct basic_block {
+    size_t ref_count;
+
     struct {
-        struct operation** buffer;
+        operation_t** buffer;
         size_t len;
         size_t cap;
     } operations;
@@ -27,12 +31,14 @@ void basic_block_drop(struct basic_block** self);
 
 bool basic_block_is_empty(const struct basic_block* self);
 
-void basic_block_push_operation(struct basic_block* self, struct operation* op);
+void basic_block_push_operation(struct basic_block* self, operation_t* op);
+
+const operation_t* basic_block_get_operation(const struct basic_block* self, size_t idx);
 
 void basic_block_resize_links(struct basic_block* self, size_t new_size);
 
 void basic_block_set_link(struct basic_block* self, struct basic_block* bb, size_t idx);
 
-struct basic_block* basic_block_split_end(struct basic_block* self);
+void basic_block_append_operations(struct basic_block* self, struct basic_block* donor);
 
 #endif /* __ENTITY_BASIC_BLOCK_H__ */
